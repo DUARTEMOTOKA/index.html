f4af2fc8117d0ac9c04923072736376e6d49a05d
DUARTEMOTOKA <duartedasilvai3@gmail.com>
2026-09-14 23:01:42 -0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "br.com.duartemotoka.numeracerto"
+
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "br.com.duartemotoka.numeracerto"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.webkit:webkit:1.12.1")
+}
